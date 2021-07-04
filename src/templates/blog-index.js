3165a3210d2d7efc802c35a65b1a5f4c4facedb5
dkp1903 <dushyantpathak6789@gmail.com>
2021-07-04 19:08:46 +0000

    refactor:indx-content

--- src/templates/blog-index.js
+++ src/templates/blog-index.js
@@ -19,71 +19,35 @@ class BlogIndexTemplate extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO />
-        <aside>
-          <Bio />
-        </aside>
-        <main>
-                        <nav
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    minHeight: '10vh',
-
-                }}
-            >
-                {/* <ul
-                    style={{
-                        width: '80%',
-                        display: 'flex',
-                        justifyContent: 'space-around',
-                        alignItems: 'center',
-                        listStyle: 'none',
-                        marginLeft: '10%'
-                    }}
-                >
-                    <li>
-                    <Link to="/Tech">Sofware Development</Link></li>
-                    <li><Link to="/Data-Structures">DSA</Link></li>
-                    <li><Link to="/Markets">Markets</Link></li>
-                    <li><Link to="/productivity">Productivity</Link></li>
-                </ul> */}
-            </nav>
-      <p>
-        Welcome to DKProbes. I am Dushyant(I prefer being called DKP, because it's so much easier to pronounce), a Software Engineer by profession. I write regularly about <a href="https://dkprobes.tech/Tech">Full Stack Development</a>, <a href="https://dkprobes.tech/Data-Structures">Data Structures and Algorithms</a> and <a href="https://dkprobes.tech/productivity">Productivity</a>, and more occasionally, about
-         Markets, Foreign Affairs and my experiences working with the companies I have interned with.
-        <br /> <br />
-        I also occasionally send out newsletters of my writings, so if that interests you, subscribe with your emails below. If not, continue reading, by all means!
+          <SEO />
+            <aside>
+                <Bio />
+            </aside>
+            <main>             
+                <p>
+                    Welcome to DKProbes. I am Dushyant(DKP), a Full Stack Engineer. I love writing about challenging problems that I come across and/or face either in my work life, my personal projects, or stuff that I read/research on.
+                    <br />
+                    My posts(I call them probes), are intuitive, focused, sarcastic and focus on retention rather than quantity and overwhelming content.
+                    <br />
+                    You can find stuff on Data Structures and Algorithms, Full Stack Development, some market analyses and some experiences about the non-tech aspect of the software engineering world. 
+       
 <h2 style={{color: "#6875f5"}}>Tech</h2>
 <u>
 <a href="https://dkprobes.tech/ds">
-<h3 style={{color: "#6875f5", padding: "5px 40px"}}> Data Structures and Algorithms </h3></a> 
+<h3 style={{color: "#6875f5", padding: "5px 40px"}}>DSA</h3></a> 
 </u>
 <p style={{padding: "0px 40px"}}>
-The world runs on problem solvers, and software companies around the world are yet to find a better metric to test problem solving skills, than the much loved and equally dreaded 
-Data structures and Algorithms. Having been through more than 15 interviews for startups like Crio.Do, Oye Rickshaw, big tech like Amazon and non tech firms like Deutsche Bank, I have gained 
-quite some experience in figuring out the major points that drive interviews, and what the right approach to learn would be.
-
-In my DSA probes, I follow a seven step approach : 
-<ol style={{ padding: "5px 40px"}}>
-<li>Pick up a problem statement - usually from Codeforces/GeeksforGeeks/Leetcode or similar platforms</li>
-<li>Analyze the requirements, edge cases and expected approach in depth</li>
-<li>Explain my thought process step by step as if it were an actual interview</li>
-<li>Start with a brute force solution. Explain why it's not the best solution</li>
-<li>Improve upon the solution iteratively to reach the best case scenario</li>
-<li>Give the code for the solution in C++(I intend to add Java/Python/Javascript language support later, with support of the community)</li>
-<li>List out the companies where this problem had been asked, links to the GeeksforGeeks/Leetcode versions of the problem.</li>
-</ol>
+I've lived long and seen enough in the software industry to realize that you can't get a job without knowing DSA, and so, rather than forcing myself to do it, I try to develop interest in it by writing about my thought process.
+I do NOT intend to make this a complete end-to-end resource for every DSA problem in the world. I only write about problems that stimulated my thinking, or that I considered important, and I write about them in a way that could help you
+retain the approaches better.
 </p>
 <u>
-<a href="https://dkprobes.tech/Tech">
-<h3 style={{size: '30dp', padding: "5px 40px", color: "#6875f5"}} >Software Development</h3></a> 
+<a href="https://dkprobes.tech/sen/tech">
+<h3 style={{size: '30dp', padding: "5px 40px", color: "#6875f5"}} >Full Stack Development</h3></a> 
 </u>
 <p style={{padding: "0px 40px"}}>
-Tech is best learnt when it's explained so that it's hands on, retention-first and focused - and that's what I bring to you. 
-I have been developing full stack applications in Javascript, Python and related technologies 
-for more than 3 years now, and regularly learn new technologies, and architectures, and write about the same, so if that interests you, do check this out. 
+      Full Stack Development is what's known as firefighting by software engineering veterans, or so I've been told. No, really - software engineering is always about some idiot out there messing something up, and you rushing with your firehose(read, your 'machine') to try
+      and deal with it as best as you can. I keep running into fires all over the place, and I keep picking up a few tips and tricks of the trade - which include some concepts, tutorials and hacks. 
 <br /><br />
 
 
