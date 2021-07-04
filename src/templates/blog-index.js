06a400c2a99c739b5a8fe987e75985ccf8c78306
dkp1903 <dushyantpathak6789@gmail.com>
2021-07-04 19:55:24 +0000

    refactor:indx-content

--- src/templates/blog-index.js
+++ src/templates/blog-index.js
@@ -31,41 +31,44 @@ class BlogIndexTemplate extends React.Component {
                     <br />
                     You can find stuff on Data Structures and Algorithms, Full Stack Development, some market analyses and some experiences about the non-tech aspect of the software engineering world. 
        
-<h2 style={{color: "#6875f5"}}>Tech</h2>
 <u>
 <a href="https://dkprobes.tech/ds">
-<h3 style={{color: "#6875f5", padding: "5px 40px"}}>DSA</h3></a> 
+<h3 style={{color: "#6875f5", padding: "5px 20px"}}>DSA</h3></a> 
 </u>
-<p style={{padding: "0px 40px"}}>
+<p style={{padding: "0px 20px"}}>
 I've lived long and seen enough in the software industry to realize that you can't get a job without knowing DSA, and so, rather than forcing myself to do it, I try to develop interest in it by writing about my thought process.
 I do NOT intend to make this a complete end-to-end resource for every DSA problem in the world. I only write about problems that stimulated my thinking, or that I considered important, and I write about them in a way that could help you
 retain the approaches better.
 </p>
 <u>
 <a href="https://dkprobes.tech/sen/tech">
-<h3 style={{size: '30dp', padding: "5px 40px", color: "#6875f5"}} >Full Stack Development</h3></a> 
+<h3 style={{size: '30dp', padding: "5px 20px", color: "#6875f5"}} >Full Stack Development</h3></a> 
 </u>
-<p style={{padding: "0px 40px"}}>
+<p style={{padding: "0px 20px"}}>
       Full Stack Development is what's known as firefighting by software engineering veterans, or so I've been told. No, really - software engineering is always about some idiot out there messing something up, and you rushing with your firehose(read, your 'machine') to try
       and deal with it as best as you can. I keep running into fires all over the place, and I keep picking up a few tips and tricks of the trade - which include some concepts, tutorials and hacks. 
-<br /><br />
-
 
 </p>
-<h2 style={{color: "#6875f5"}}>Non Tech</h2>
+<u>
+<a href="https://dkprobes.tech/sen/experience">
+<h3 style={{size: '30dp', padding: "5px 20px", color: "#6875f5"}} >The world of software engineering - experiences</h3></a> 
+</u>
+<p style={{padding: "0px 20px"}}>
+      Here, I write about some common concepts, processes, reviews and issues in the world of software engineering. I also keep note of my experiences working at each of the companies I have worked with.
+</p>
 
 <a href="https://dkprobes.tech/sen/productivity">
-<h3 style={{size: '30dp', padding: "5px 40px", color: "#6875f5"}} >Productivity</h3></a> 
-<p style={{padding: "0px 40px"}}>
+<h3 style={{size: '30dp', padding: "5px 20px", color: "#6875f5"}} >Productivity</h3></a> 
+<p style={{padding: "0px 20px"}}>
 I routinely experiment with different productivity tools and techniques and write about the experiences, so if that interests you, check this out!
 </p>
+
 <a href="https://dkprobes.tech/analysis">
-<h3 style={{color: "#6875f5", padding: "5px 40px"}}> Others </h3></a> 
+<h3 style={{color: "#6875f5", padding: "5px 20px"}}> Analysis </h3></a> 
 <p style={{padding: "5px 40px"}}>
 I analyze markets and business strategy in depth, and bring to you, some of 
 the hottest stories in the market, from complete case studies of WeWork, Uber to 
-analyses of stocks, and corporate concepts, my personal internship experiences, events of international importance and more!
-You can find all my posts on markets <Link to="/Markets">here</Link>
+analyses of stocks, and corporate concepts.
 </p>
 
 </p>
